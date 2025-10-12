b8291f096fa5af314af7cf26eb16320f20ac828e
Dori Fussmann <dori@thenightventures.com>
2025-10-12 15:43:51 -0700

    v1

--- app/agent-kit/page.tsx
+++ app/agent-kit/page.tsx
@@ -2,12 +2,17 @@
 
 import { ChatKit } from '@openai/chatkit-react';
 import { useChatkit } from '@/features/chatkit/useChatkit';
+import { useEffect } from 'react';
 
 export default function AgentKitPage() {
   const { control } = useChatkit();
   
+  useEffect(() => {
+    console.log('ChatKit control:', control);
+  }, [control]);
+  
   return (
-    <div className="h-[calc(100vh-64px)] w-full">
+    <div className="h-[calc(100vh-64px)] w-full bg-gray-100">
       <ChatKit control={control} />
     </div>
   );
